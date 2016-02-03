05c735b487d975f696258c0fb08bc05fff69229f
TaunoLainevool <TaunoLainevool@users.noreply.github.com>
2016-02-03 21:50:21 +0200

    Done

--- clock.js
+++ clock.js
@@ -1,6 +1,6 @@
 window.onload = function(){
-var clock= document.getElementById("clock").style.color = "yellow";
-
+var clock= document.getElementById("clock").style.color = "white";
+var color = document.getElementById("color").style.color="white";
 
 
     //käivitan intervalli/500= 500 millisekundit = 0,5s
@@ -10,15 +10,54 @@ var clock= document.getElementById("clock").style.color = "yellow";
     }, 500);
 
 
-    window.addEventListener('keyup', function(event){
-     document.getElementById("clock").style.color = "red";
-     if(event.which == 13){
 
-       document.getElementById("clock").style.color = "yellow";
-     }
-     });
+      window.addEventListener('keyup', function(event){
+        document.getElementById("clock").style.color = "red";
+      });
+      window.addEventListener('click', function(event){
+        document.getElementById("clock").style.color = "white";
+      });
+
+
+      var big = document.getElementById("big");
+      var small= document.getElementById("small");
+      var same= document.getElementById("same");
+      //lisan nuppudele kuularid
+
+      document.getElementById("bigger").addEventListener("click",bigger);
+      document.getElementById("smaller").addEventListener("click",function(){
+        document.getElementById("clock").style.fontSize = "medium";
+      });
+      document.getElementById("same").addEventListener("click",function(){
+        document.getElementById("clock").style.fontSize = "x-large";
+      });
+
+
+
+
+
+
      document.body.style.background = "darkblue";
-     document.body.style.backgroundAttachment = "center";
+
+     document.getElementById("name").style.fontFamily = "Verdana";
+     document.getElementById("name").style.fontSize = "large";
+     document.getElementById("name").style.textAlign = "center";
+
+     document.getElementById("clock").style.borderWidth = "inherit";
+     document.getElementById("color").style.fontFamily = "Verdana";
+     document.getElementById("color").style.fontSize = "large";
+     document.getElementById("color").style.textAlign = "center";
+
+
+
+     document.getElementById("clock").style.borderRadius = "25px";
+     document.getElementById("clock").style.border = "thick solid #0000FF";
+     document.getElementById("clock").style.borderWidth = "5px 20px 20px 20px";
+     document.getElementById("clock").style.borderColor="yellow";
+     document.getElementById("clock").style.textAlign = "center";
+     document.getElementById("clock").style.fontFamily = "Impact,Charcoal,sans-serif";
+     document.getElementById("clock").style.fontSize = "x-large";
+
 };
 
 //võtab aja ja kirjutab aja clock elemendi sisse
@@ -31,19 +70,20 @@ function writeDate(){
   var minutes= today.getMinutes("Min");
   var seconds = today.getSeconds("Sec");
   clock.innerHTML = addZeroBefore(hours) + ":" +addZeroBefore(minutes)+":"+addZeroBefore(seconds)+" <br> "+day+"."+ days[today.getDay()]+"."+year;
-
-
 }
+
+
  function addZeroBefore(number){
 
 
   if (number<10){
     number= "0"+number;
 }
-
    return number;
  }
 
- function event() {
 
+ function bigger(){
+
+     document.getElementById("clock").style.fontSize = "xx-large";
  }
