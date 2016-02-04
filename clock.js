5c2b8b6c73ef91fa448332f5bfc82d22338ea587
Priit Puru <priit.puru@adm.ee>
2016-02-04 18:34:49 +0200

    HW-1 Styled. Added day/month/year. Added an action button for more functions.

--- clock.js
+++ clock.js
@@ -1,11 +1,16 @@
 window.onload = function(){ //assign divs to variables after page has finished loading
-		var hr = document.getElementById("hr");
+		var d = new Date();
+		var time = document.getElementById("time");
 		var min = document.getElementById("min");
 		var sec = document.getElementById("sec");
+		var date = document.getElementById("date");
+		//arrays for string values of dates
+		var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+		var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 	setInterval(function(){ //update every second
-		var d = new Date();
-		hr.innerHTML = '<p class="clock__number--nopadding">'+d.getHours()+'</p>';
-		min.innerHTML = '<p class="clock__number--nopadding">'+d.getMinutes()+'</p>';
-		sec.innerHTML = '<p class="clock__number--nopadding">'+d.getSeconds()+'</p>';
+		d = new Date();
+		time.innerHTML = '<p class="clock__number--nopadding">'+d.getHours()+'/'+d.getMinutes()+'/'+d.getSeconds()+'</p>';
+		return d;
 	},1000);
+	date.innerHTML = day[d.getDay()]+'/'+month[d.getMonth()]+'/'+d.getFullYear();
 }
