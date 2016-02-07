ca195840fdd8e7c30cd7d897a438fed2b033332f
Jarmo Habakuk <jarmhab@greeny.cs.tlu.ee>
2016-02-07 12:36:03 +0200

    added lesson files

---
+++ clock.js
@@ -0,0 +1,40 @@
+window.onload = function (){
+
+var clock = document.getElementById('clock');
+
+writeDate(); //selleks, et ei oleks algul näha 0:0:0
+
+  //käivitan intervalli
+window.setInterval(function(){
+  //iga 500ms tagant käivitan writeDate'i
+  writeDate();
+
+}, 500);
+
+writeDate();
+
+
+};
+
+//võtab aja ja kirjutab #clock elemendi siss
+function writeDate(){
+
+  var today =new Date();
+
+  var hours = today.getHours();
+  var minutes = today.getMinutes();
+  var seconds = today.getSeconds();
+
+
+clock.innerHTML = addZeroBefore(hours) + ":" + addZeroBefore(minutes) + ":" + addZeroBefore(seconds);
+
+}
+
+function addZeroBefore(number){
+
+  if(number < 10){
+    number = "0" + number;
+  }
+
+  return number;
+}
