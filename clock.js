6080347c7c617ac825188917ce9c87910c24f1ff
Jarmo Habakuk <jarmhab@greeny.cs.tlu.ee>
2016-02-07 18:21:57 +0200

    added date

--- clock.js
+++ clock.js
@@ -3,7 +3,6 @@ window.onload = function (){
 var clock = document.getElementById('clock');
 
 
-
   //käivitan intervalli
 window.setInterval(function(){
   //iga 500ms tagant käivitan writeDate'i
@@ -16,19 +15,23 @@ writeDate();
 
 };
 
+
+
 //võtab aja ja kirjutab #clock elemendi siss
 function writeDate(){
 
-  var today =new Date();
+  var today = new Date();
 
-
+  var year = today.getFullYear();
+  var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
+  var month = months[today.getMonth()];
+  var day = today.getDate();
   var hours = today.getHours();
   var minutes = today.getMinutes();
   var seconds = today.getSeconds();
 
 
-clock.innerHTML = addZeroBefore(hours) + ":" + addZeroBefore(minutes) + ":" + addZeroBefore(seconds);
-
+clock.innerHTML = addZeroBefore(hours) + ":" + addZeroBefore(minutes) + ":" + addZeroBefore(seconds) + "<br>" + (day) + "." + (month) + "." + (year);
 
 }
 
