7eefd241dd70596afd414a898ba80f7941ff0124
richardroom <richard.roomusaar@gmail.com>
2016-02-08 22:32:25 +0200

    Kella põhi
    Kell saab arvutist kätte aja.

---
+++ clock.js
@@ -0,0 +1,34 @@
+window.onload = function(){
+writeDate();
+   var clock = document.getElementById('clock');
+   window.setInterval(function(){
+writeDate();
+
+   }, 1000 //millisekundid
+ );
+
+
+ };
+
+function writeDate(){
+
+  //Kellaaeg arvutist
+  var today = new Date();
+
+  var hours = setZeroBefore(today.getHours());
+  var minutes = setZeroBefore(today.getMinutes());
+  var seconds = setZeroBefore(today.getSeconds());
+
+  clock.innerHTML = hours + ':'+ minutes + ':' + seconds;
+
+}
+
+
+//funktsioon 0-i lisamiseks
+function setZeroBefore(number){
+  if(number < 10){
+number = '0' + number;
+
+  }
+return number;
+}
