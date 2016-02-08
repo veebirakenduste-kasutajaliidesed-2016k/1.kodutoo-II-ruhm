6ebd88af19fed510e7107ff151ea8c18b30dc91a
Raiko Lepik <raikolepik@users.noreply.github.com>
2016-02-08 08:42:55 +0200

    kell t66tab ilusti

--- clock.js
+++ clock.js
@@ -1,120 +1,45 @@
-//ootan kuni leht on laetud
+window.onload = function() {
 
-window.onload= function(){
+  var clock = document.getElementById("clock");
 
-  var clock = document.getElementById('clock').style.color="Blue";
+  var roundhour = document.getElementById("roundhour");
+  var roundminute = document.getElementById("roundminute");
+  var roundsecond = document.getElementById("roundsecond");
 
-  window.setInterval(function(){
-//iga 500ms tagant kĆ¤ivitan writeDateĀ´i
+  writeDate();
+
+  window.setInterval(function() {
     writeDate();
-
 
   }, 500);
 
-  /*
- * Starts any clocks using the user's local time
- * From: cssanimation.rocks/clocks
- */
-function initLocalClocks() {
-  // Get the local time using JS
-  var date = new Date;
-  var seconds = date.getSeconds();
-  var minutes = date.getMinutes();
-  var hours = date.getHours();
-
-  // Create an object with each hand and it's angle in degrees
-  var hands = [
-    {
-      hand: 'hours',
-      angle: (hours * 30) + (minutes / 2)
-    },
-    {
-      hand: 'minutes',
-      angle: (minutes * 6)
-    },
-    {
-      hand: 'seconds',
-      angle: (seconds * 6)
-    }
-  ];
-  // Loop through each of these hands to set their angle
-  for (var j = 0; j < hands.length; j++) {
-    var elements = document.querySelectorAll('.' + hands[j].hand);
-    for (var k = 0; k < elements.length; k++) {
-        elements[k].style.webkitTransform = 'rotateZ('+ hands[j].angle +'deg)';
-        elements[k].style.transform = 'rotateZ('+ hands[j].angle +'deg)';
-        // If this is a minute hand, note the seconds position (to calculate minute position later)
-        if (hands[j].hand === 'minutes') {
-          elements[k].parentNode.setAttribute('data-second-angle', hands[j + 1].angle);
-        }
-    }
-  }
-}
-
-
 };
-//vĆµtab aja ja kirjutab #clock elemendi sisse
-function writeDate(){
-  var today = new Date();
 
-  var hours = today.getHours();
 
-  var minutes = today.getMinutes();
+function writeDate() {
 
-  var seconds = today.getSeconds();
+  var d = new Date();
+  var minute = d.getMinutes();
+  var hour = d.getHours();
+  var second = d.getSeconds();
 
-  //muudan #clock elemendi hmtli
-    clock.innerHTML = addZeroBefore(hours) + ':' + addZeroBefore(minutes) + ':' + addZeroBefore(seconds);
+  clock.innerHTML = ("Kellaaeg on: " + addZeroBefore(hour) + ":" + addZeroBefore(minute) + ":" + addZeroBefore(second));
+
+  var roundsecondloc = second * 6;
+  var roundminuteloc = minute * 6;
+  var roundhourloc = hour * 30 + minute / 2.5;
+
+  roundsecond.style.transform = "rotate(" + roundsecondloc + "deg)";
+  roundminute.style.transform = "rotate(" + roundminuteloc + "deg)";
+  roundhour.style.transform = "rotate(" + roundhourloc + "deg)";
 
 }
 
+function addZeroBefore(number) {
 
-function addZeroBefore(number){
-  if(number < 10){
+  if(number < 10) {
     number = "0" + number;
-
-
-
   }
 
   return number;
-
-
-}
-/*
- * Set a timeout for the first minute hand movement (less than 1 minute), then rotate it every minute after that
- */
-function setUpMinuteHands() {
-  // Find out how far into the minute we are
-  var containers = document.querySelectorAll('.minutes-container');
-  var secondAngle = containers[0].getAttribute("data-second-angle");
-  if (secondAngle > 0) {
-    // Set a timeout until the end of the current minute, to move the hand
-    var delay = (((360 - secondAngle) / 6) + 0.1) * 1000;
-    setTimeout(function() {
-      moveMinuteHands(containers);
-    }, delay);
-  }
-}
-
-/*
- * Do the first minute's rotation
- */
-function moveMinuteHands(containers) {
-  for (var i = 0; i < containers.length; i++) {
-    containers[i].style.webkitTransform = 'rotateZ(6deg)';
-    containers[i].style.transform = 'rotateZ(6deg)';
-  }
-  // Then continue with a 60 second interval
-  setInterval(function() {
-    for (var i = 0; i < containers.length; i++) {
-      if (containers[i].angle === undefined) {
-        containers[i].angle = 12;
-      } else {
-        containers[i].angle += 6;
-      }
-      containers[i].style.webkitTransform = 'rotateZ('+ containers[i].angle +'deg)';
-      containers[i].style.transform = 'rotateZ('+ containers[i].angle +'deg)';
-    }
-  }, 60000);
 }
