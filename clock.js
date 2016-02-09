2128793a02344b8ddd186c2e9674501eb0f7560f
paul657 <paul657@users.noreply.github.com>
2016-02-10 01:54:31 +0200

    Update vol2 clock.js
    first final version

--- clock.js
+++ clock.js
@@ -1,6 +1,8 @@
  window.onload = function(){
   loadClock();
-  window.setInterval(function(){loadClock();}, 100);
+  loadDate();
+  document.getElementById("date-div").style.opacity = "0";
+  window.setInterval(function(){loadClock();loadDate();}, 500);
 
   var scroll = document.getElementById("page");
   if (scroll.addEventListener) {
@@ -8,54 +10,95 @@
     scroll.addEventListener("DOMMouseScroll", MouseWheelHandler, false);
   }
   else scroll.attachEvent("onmousewheel", MouseWheelHandler);
-
-  var fsize = 10;
-  function MouseWheelHandler(e) {
-  	var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
-    fsize = Math.max(10, Math.min(400, fsize + (10 * delta)));
-    //console.log(fsize);
-    document.getElementById("clock-div").style.fontSize = fsize + "px";
-    divPlacing();
-    return false;
-  }
-
-  var x = event.keyCode;
-  console.log(x);
-
 };
 
+//kella laadimine
 function loadClock(){
   var clock = new Date();
   var hour = addZero(clock.getHours());
   var minute = addZero(clock.getMinutes());
   var second = addZero(clock.getSeconds());
-  var clockDiv = document.getElementById('clock-div');
-  clockDiv.innerHTML = hour + ":" + minute + ":" + second;
-  divPlacing();
+  document.getElementById('clock-div').innerHTML = hour + ":" + minute + ":" + second;
 }
 
+//kuupäeva laadimine
+function loadDate(){
+  var clock = new Date();
+  var day = addZero(clock.getDate());
+  var month = addZero((clock.getMonth())+1);
+  var year = clock.getFullYear();
+  document.getElementById('date-div').innerHTML = day + "." + month + "." + year;
+}
+
+//kuupäeva <div> elemendi paigutus
+function datePlacing(){
+  var clockDivHeight = document.getElementById('clock-div').offsetHeight;
+  document.getElementById('date-div').style.marginTop = (clockDivHeight/1.8) + "px";
+}
+
+//nulli lisamine kella ja kuupäeva numbritele
 function addZero(number){
   if (number < 10){
     number = "0" + number;
   } return number;
 }
 
-
-function divPlacing(){
-  var w = window.innerWidth;
-  var h = window.innerHeight;
-  var clockDiv = document.getElementById('clock-div');
-  var divw = clockDiv.offsetWidth;
-  var divh = clockDiv.offsetHeight;
-  clockDiv.style.position = "absolute";
-  clockDiv.style.left = (w / 2) - (divw / 2) + "px";
-  clockDiv.style.top = (h / 2) - (divh / 2) + "px";
+//toimingud hiirerullikuga
+var fsize = 30;
+function MouseWheelHandler(e) {
+  var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+  fsize = Math.max(20, Math.min(400, fsize + (10 * delta)));
+  document.getElementById("clock-div").style.fontSize = fsize + "px";
+  document.getElementById("date-div").style.fontSize = (fsize/2) + "px";
+  document.getElementById("info").style.opacity = "0";
+  document.getElementById("info").style.marginTop = "100px";
+  document.getElementById("date-div").style.opacity = "0";
+  datePlacing();
+  return false;
 }
 
+//toimingud hiireklikiga
 var counter = 0;
 function mouseClick(){
-  var fonts = ["Comic Sans MS", "Courier New", "Arial", "Georgia"];
+  var fonts = ["Comic Sans MS", "Courier New", "Arial", "Poiret One", "Impact", "Times New Roman", "Verdana"];
   counter++;
   document.getElementById("clock-div").style.fontFamily = '"' + fonts[counter] + '"';
-  if(counter == 4){counter = 0;}
+  document.getElementById("date-div").style.fontFamily = '"' + fonts[counter] + '"';
+  if(counter == 7){counter = 0;}
+  document.getElementById("info").style.marginTop = "100px";
+  document.getElementById("info").style.opacity = "0";
 }
+
+//toimingud klaviatuuriga
+document.addEventListener('keydown', function(event) {
+  document.getElementById("info").style.opacity = "0";
+  document.getElementById("info").style.marginTop = "100px";
+    if(event.keyCode == 49) {
+      document.getElementById("clock-div").style.color = "white";
+      document.getElementById("date-div").style.color = "white";
+    }
+    else if(event.keyCode == 50) {
+      document.getElementById("clock-div").style.color = "salmon";
+      document.getElementById("date-div").style.color = "salmon";
+    }
+    else if(event.keyCode == 51) {
+      document.getElementById("clock-div").style.color = "green";
+      document.getElementById("date-div").style.color = "green";
+    }
+    else if(event.keyCode == 52) {
+      document.getElementById("clock-div").style.color = "red";
+      document.getElementById("date-div").style.color = "red";
+    }
+    else if(event.keyCode == 53) {
+      document.getElementById("clock-div").style.color = "orange";
+      document.getElementById("date-div").style.color = "orange";
+    }
+    else if(event.keyCode == 68) {
+      if(document.getElementById("date-div").style.opacity == "0"){
+        document.getElementById("date-div").style.opacity = "100";
+        datePlacing();
+      }else if(document.getElementById("date-div").style.opacity == "100"){
+        document.getElementById("date-div").style.opacity = "0";
+      }
+    }
+});
