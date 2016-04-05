047a7b6d425039db7e055992ab8844a6ae5b4b8b
Tauri Kirsipuu <tauri139@users.noreply.github.com>
2016-04-05 21:39:09 +0300

    added date and time resizing

--- clock.js
+++ clock.js
@@ -2,22 +2,31 @@ window.onload = function(){
 
   var clock = document.getElementById('clock');
   var date = document.getElementById('date');
+  document.getElementById('slider').addEventListener("click", Change);
 
-  writeDate(); //selleks, et ei oleks alguses näha 0:0:0-i
+
   writeTime();
+  writeDate(); //selleks, et ei oleks alguses näha nulle näha
+  Change();
 
   window.setInterval(function(){
     writeDate();
+    writeTime();
   }, 500);
 
 };
+function Change() {
+  var fontslider = document.getElementById('slider');
+  clock.style.fontSize = fontslider.value*window.innerWidth*0.1 + 'px';
+  date.style.fontSize = fontslider.value*window.innerWidth*0.1 + 'px';
+}
 
 function writeTime(){
   var time = new Date();
+
   var hour = addZeroBefore(time.getHours());
   var min = addZeroBefore(time.getMinutes());
   var sec = addZeroBefore(time.getSeconds());
-
 
   clock.innerHTML = hour + ':'+ min + ':' + sec;
 
@@ -25,12 +34,12 @@ function writeTime(){
 
 function writeDate(){
   var time = new Date();
+  var fontslider = document.getElementById('slider');
+
   var month = addZeroBefore(time.getMonth());
   var year = addZeroBefore(time.getFullYear());
   var day = addZeroBefore(time.getDate());
-  var hour = addZeroBefore(time.getHours());
-  var min = addZeroBefore(time.getMinutes());
-  var sec = addZeroBefore(time.getSeconds());
+
   date.innerHTML = day + '/' + month + '/' + year;
 }
 
